fe5a3414613a0299ec6485b9a34769d534dc2692
andrewflnr@gmail.com <andrewflnr@gmail.com@30ee8f51-df1a-0410-9115-614dbba9287e>
2013-05-23 01:31:22 +0000

    Bump version to 1.8, and make sure CHANGES, etc, are included in source dist

--- dpkt/__init__.py
+++ dpkt/__init__.py
@@ -6,7 +6,7 @@ __author__ = 'Dug Song <dugsong@monkey.org>'
 __copyright__ = 'Copyright (c) 2004 Dug Song'
 __license__ = 'BSD'
 __url__ = 'http://dpkt.googlecode.com/'
-__version__ = '1.7'
+__version__ = '1.8'
 
 from dpkt import *
 
